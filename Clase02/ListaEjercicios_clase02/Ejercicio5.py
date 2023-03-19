b18541b99c183e9fafd3868c27cc405825a8442c
Kamivea <kamivea613@gmail.com>
2023-03-19 03:14:49 -0500

    update

--- Clase02/ListaEjercicios_clase02/Ejercicio5.py
+++ Clase02/ListaEjercicios_clase02/Ejercicio5.py
@@ -1,13 +1,14 @@
+
 import os
 path=os.path.abspath('..')
 for f in os.scandir(path):
     print(f,"=>",f.path)
     print(f.is_dir())
-def funcionListCarpeta(path):
+def funcionRecursiva(path):
     #funcion que me liste el path
     listaCarpeta=[]
     for f in listaCarpeta:
         if f.isDir():
-            funcionListCarpeta(f.path)
+            funcionRecursiva(f.path)
         else:
             print(f.path)
